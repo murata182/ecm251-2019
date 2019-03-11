2bca08406b7bb710a07c189711178d130493658d
RAFAEL  MURATA <17.00103-0@maua.br>
2019-03-11 11:38:20 -0300

    Projeto criado no NetBeans

---
+++ pratica01/meuPrimeiroProjetoComNetBeans/src/UsandoNetBeans.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 17.00103-0
+ */
+public class UsandoNetBeans {
+    public static void main(String[] args) {
+        System.out.println("Olá Mundo!");
+    }
+}
